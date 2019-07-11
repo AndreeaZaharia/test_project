2c8d24460c98772a230ae79b140a883ccf6ea52e
AndreeaZaharia <AndreeaZaharia@users.noreply.github.com>
2019-07-11 13:49:32 +0300

    login and registration complete

--- webui/src/app/pages/login/login.component.ts
+++ webui/src/app/pages/login/login.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
     model: any = {};
     errMsg:string = '';
+    isLoginForm = true;
     constructor(
         private router: Router,
         private loginService: LoginService) { }
@@ -46,9 +47,40 @@ export class LoginComponent implements OnInit {
                 }
             );
     }
+    register(){
+        this.loginService.register(this.model.username,this.model.password) .subscribe(resp => {
+                if (resp.user === undefined || resp.user.token === undefined || resp.user.token === "INVALID" ){
+                    this.errMsg = 'Username or password is incorrect';
+                    return;
+                }
+                this.router.navigate([resp.landingPage]);
+            },
+            errResponse => {
+                switch(errResponse.status){
+                    case 401:
+                        this.errMsg = 'Username or password is incorrect';
+                        break;
+                    case 404:
+                        this.errMsg = 'Service not found';
+                    case 408:
+                        this.errMsg = 'Request Timedout';
+                    case 500:
+                        this.errMsg = 'Internal Server Error';
+                    default:
+                        this.errMsg = 'Server Error';
+                }
+            }
+        );
+    }
 
     onSignUp(){
       this.router.navigate(['signup']);
+    }
+    showRegistrationForm():void{
+        this.isLoginForm = false;
+    }
+    showLoginForm():void{
+        this.isLoginForm = true;
     }
 
 
--- webui/src/app/services/api/login.service.ts
+++ webui/src/app/services/api/login.service.ts
@@ -8,6 +8,7 @@ import { UserInfoService, LoginInfoInStorage} from '../user-info.service';
 import { ApiRequestService } from './api-request.service';
 
 export interface LoginRequestParam{
+    userId:string;
     username:string;
     password:string;
 }
@@ -27,9 +28,10 @@ export class LoginService {
         let me = this;
 
         let bodyData:LoginRequestParam = {
+            "userId":username,
             "username": username,
             "password": password,
-        }
+        };
         /*
             Using BehaviorSubject instead of Subject
             In Angular services are initialized before the components, if any component is
@@ -82,7 +84,57 @@ export class LoginService {
 
             return loginDataSubject;
     }
+    register(username:string, password:string): Observable<any> {
+        let me = this;
 
+        let bodyData:LoginRequestParam = {
+            "userId": username,
+            "username": username,
+            "password": password,
+        };
+
+        let loginDataSubject:BehaviorSubject<any> = new BehaviorSubject<any>([]); // Will use this BehaviorSubject to emit data that we want after ajax login attempt
+        let loginInfoReturn:LoginInfoInStorage; // Object that we want to send back to Login Page
+
+        this.apiRequest.post('user', bodyData)
+            .subscribe(jsonResp => {
+                    if (jsonResp !== undefined && jsonResp !== null && jsonResp.operationStatus === "SUCCESS"){
+                        //Create a success object that we want to send back to login page
+                        loginInfoReturn = {
+                            "success"    : true,
+                            "message"    : jsonResp.operationMessage,
+                            "landingPage": this.landingPage,
+                            // "user"       : {
+                            //     "userId"     : jsonResp.item.userId,
+                            //     "email"      : jsonResp.item.emailAddress,
+                            //     "displayName": jsonResp.item.firstName + " " + jsonResp.item.lastName,
+                            //     "token"      : jsonResp.item.token,
+                            // }
+                        };
+
+                        // store username and jwt token in session storage to keep user logged in between page refreshes
+                        this.userInfoService.storeUserInfo(JSON.stringify(loginInfoReturn.user));
+                    }
+                    else {
+                        //Create a faliure object that we want to send back to login page
+                        loginInfoReturn = {
+                            "success":false,
+                            "message":jsonResp.msgDesc,
+                            "landingPage":"/login"
+                        };
+                    }
+                    loginDataSubject.next(loginInfoReturn);
+                },
+                err => {
+                    loginInfoReturn = {
+                        "success": false,
+                        "message": err.url + " >>> " + err.statusText +  "[" + err.status +"]",
+                        "landingPage": "/login"
+                    };
+                });
+
+        return loginDataSubject;
+    }
     logout(navigatetoLogout=true): void {
         // clear token remove user from local storage to log user out
         this.userInfoService.removeUserInfo();
